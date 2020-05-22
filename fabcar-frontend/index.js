2a4e50340aaa3cc27588dbda3dfad51bee19447b
igna1998 <ignacio.martnezrodrguez@gmail.com>
2020-05-22 17:53:17 +0200

    Añadida primera aproximación a firmar transacciones offline

---
+++ fabcar-frontend/index.js
@@ -0,0 +1,38 @@
+/*
+    Converted to front end code with browserify
+*/
+
+
+var elliptic = require('elliptic');
+var util = require('util');
+const { KEYUTIL } = require('jsrsasign');
+
+
+function _preventMalleability(sig) {
+	const halfOrder = elliptic.curves.p256.n.shrn(1);
+	if (sig.s.cmp(halfOrder) === 1) {
+		const bigNum = elliptic.curves.p256.n;
+		sig.s = bigNum.sub(sig.s);
+	}
+	return sig;
+}
+
+
+window.signTransaction = function(digest, priv){
+
+    const { prvKeyHex } = KEYUTIL.getKey(priv);
+
+	const EC = elliptic.ec;
+	
+	const ecdsaCurve = elliptic.curves['p256'];
+	const ecdsa = new EC(ecdsaCurve);
+	const signKey = ecdsa.keyFromPrivate(prvKeyHex, 'hex');
+	var sig = ecdsa.sign(Buffer.from(digest, 'hex'), signKey);
+	
+	sig = _preventMalleability(sig);
+	
+    const signature = Buffer.from(sig.toDER());
+    
+    return signature;
+}
+
